c5357ebbd00d0f876dc197fe9fe3d4bfed53466a
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-08-06 12:14:02 -0600

    Changes whole apprach to game logic

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-//query selectors and event handlers
-document.addEventListener('click',boxSelected)
-
-
-
-function boxSelected(event) {
-  if(event.target.matches('.game-box')) {
-     tilePlacment()
-  }
-}
-function tilePlacment(){
-  var id = event.target.id
-  if(!gameBoard[id]) {
-    gameBoard[id] = currentPlayer
-    event.target.innerText = currentPlayer
-  }
-
-if(currentPlayer === player1.id ) {
- event.target.innerText = currentPlayer
- var id = event.target.id
-  currentPlayer = player2.id
-} else {
-   var id = event.target.id
-  event.target.innerText = currentPlayer
-  currentPlayer = player1.id
-}
-}
+// //query selectors and event handlers
+// document.addEventListener('click',boxSelected)
+//
+//
+//
+// function boxSelected(event) {
+//   if(event.target.matches('.game-box')) {
+//      tilePlacment()
+//   }
+// }
+// function tilePlacment(){
+//   var id = event.target.id
+//   if(!gameBoard[id]) {
+//     gameBoard[id] = currentPlayer
+//     event.target.innerText = currentPlayer
+//   }
+//
+// if(currentPlayer === player1.id ) {
+//  event.target.innerText = currentPlayer
+//  var id = event.target.id
+//   currentPlayer = player2.id
+// } else {
+//    var id = event.target.id
+//   event.target.innerText = currentPlayer
+//   currentPlayer = player1.id
+// }
+// }
