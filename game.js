46a86e5b50dae210528668b5c8f66810195df794
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-08-06 18:17:29 -0600

    Adds more HTML elements and now the change player function works

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 //Game Data
 
-var gameBoard = [0,1,2,3,4,5,6,7,8]
+var gameBoard = ['','','','','','','','','']
 var player1 = new Player('one','O',true)
 var player2 = new Player('two','X',false)
 var currentPlayer = player1.token
@@ -15,21 +15,23 @@ function switchCurrentPlayer() {
     currentPlayer = player2.token
     player1.isCurrentPlayer = false
     player2.isCurrentPlayer = true
-    console.log(currentPlayer)
   } else {
     currentPlayer = player1.token
     player1.isCurrentPlayer = true
     player2.isCurrentPlayer = false
-    console.log(currentPlayer)
   }
 }
 
-function selectBox(gameBoardIndex) {
-
+function selectBox(box,index) {
+  gameBoard[index] = currentPlayer
 }
 
-
-
+// selectBox('b9',9)
+// switchCurrentPlayer()
+// selectBox('b3',3)
+// switchCurrentPlayer()
+// selectBox('b0',0)
+// switchCurrentPlayer()
 
 
 
@@ -47,9 +49,3 @@ var winCons  = [
   [0,4,8],
   [2,4,6]
 ]
-
-
-
-function gameStart() {
-
-}
