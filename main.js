60433cd3c207aae3e43cceddfd85a3a12235511d
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-08-09 16:57:32 -0600

    Adds some styling and win counter

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.addEventListener('load',domClear)
 var currentPlayerStatus = document.querySelector('.turn-status')
 var gameBoxes = document.querySelectorAll('.box')
+var player1Score = document.querySelector('.player-1-score')
+var player2Score = document.querySelector('.player-2-score')
 for(var i = 0; i < gameBoxes.length; i++) {
     addEventListener('click',checksLegalMove)
 }
-
-
-var player1 = new Player('The Power Rangers','Its The Rangers Move!','<img class="player-2-token" src="assets/player1 token.gif">')
+var player1 = new Player('The Power Rangers','Its The Rangers Move!','<img class="player-1-token" src="assets/player1 token.gif">')
 var player2 = new Player('The Ninja Turtles','Its The Turtles Move!','<img class="player-2-token" src="assets/turtleToken.gif">')
 var nuGame =  new Game(player1,player2)
 
@@ -25,8 +25,13 @@ var gameIndex = gameBoxes[i]
 gameIndex.innerText = ''
   }
 }
-
-
+function changeDomScore() {
+  if(nuGame.isWinner === 'The Power Rangers') {
+  player1Score.innerText = nuGame.currentPlayer.wins
+} else if(nuGame.isWinner === 'The Ninja Turtles' ){
+  player2Score.innerText = nuGame.currentPlayer.wins
+  }
+}
 function checksLegalMove(event) {
   var  id = event.target.id
   if(nuGame.gameBoard[id] != '') {
@@ -43,6 +48,7 @@ function boxChoice(event) {
   nuGame.selectBox(id)
 if(nuGame.isWinner === nuGame.currentPlayer.name) {
    currentPlayerStatus.innerText = nuGame.currentPlayer.name + ' Win!'
+   changeDomScore()
    setTimeout(domClear,3000)
    nuGame.gameBoard = ['','','','','','','','','']
  } else if(nuGame.isWinner === 'draw') {
